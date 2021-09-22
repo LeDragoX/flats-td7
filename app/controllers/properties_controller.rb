5a22e26ab7c1e358bb8de27c0deb1402e886e5af
Plínio Larrubia <plinio2xd@gmail.com>
2021-09-22 18:05:19 -0300

    Finish 4th Session

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,4 +3,14 @@ class PropertiesController < ApplicationController
         @property = Property.find(params[:id])
     end
     
+    def new
+        @property = Property.new()
+    end
+
+    def create
+        property = Property.create(params.require(:property).permit(:title, :description, :rooms, :parking_slot, :bathrooms, :pets, :daily_rate))
+
+        redirect_to property_path(property.id)
+    end
+
 end
