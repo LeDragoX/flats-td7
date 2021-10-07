6e7d663440fc4b375ca4d8f27ebfa7b6902a9270
Plínio Larrubia <plinio2xd@gmail.com>
2021-10-07 16:47:34 -0300

    Add user tests (same as property_owners)

--- spec/system/visitor_log_in_spec.rb
+++ spec/system/visitor_log_in_spec.rb
@@ -9,7 +9,7 @@ describe 'Visitor log in' do
             # ACT
             visit root_path
             click_on 'Entrar como Proprietário do Imóvel'
-            fill_in 'Email', with: property_owner.email 
+            fill_in 'E-mail', with: property_owner.email 
             fill_in 'Senha', with: property_owner.password
             within 'form' do
                 click_on 'Entrar'
@@ -19,7 +19,7 @@ describe 'Visitor log in' do
             expect(page).to have_content('Login efetuado com sucesso!')
             expect(page).to have_content(property_owner.email)
             expect(page).to have_link('Logout')
-            expect(page).not_to have_link('Entrar como Proprietário do Imóvel')
+            expect(page).to_not have_link('Entrar como Proprietário do Imóvel')
         end
 
         it "and Logs out" do
@@ -37,7 +37,6 @@ describe 'Visitor log in' do
             expect(page).to_not have_link('Logout')
             expect(page).to have_link('Entrar como Proprietário do Imóvel')
             expect(page).to_not have_link('Cadastrar Imóvel')
-
         end
         
         it "and create an account" do 
@@ -46,7 +45,40 @@ describe 'Visitor log in' do
     
     context "as user" do
         it 'successfully' do
+            # ARRANGE
+            user = User.create!(email: 'user@test.com', password: '123456789')
+
+            # ACT
+            visit root_path
+            click_on 'Entrar como Locador'
+            fill_in 'E-mail', with: user.email 
+            fill_in 'Senha', with: user.password
+            within 'form' do
+                click_on 'Entrar'
+            end
+
+            # ASSERT
+            expect(page).to have_content('Login efetuado com sucesso!')
+            expect(page).to have_content(user.email)
+            expect(page).to have_link('Logout')
+            expect(page).to_not have_link('Entrar como Locador')
+            expect(page).to_not have_link('Cadastrar Imóvel')
+        end
+
+        it "and Logs out" do
+            # ARRANGE
+            user = User.create!(email: 'user@test.com', password: '123456789')
+
+            # ACT
+            login_as(user, scope: :user)
+            visit root_path
+            click_on 'Logout'
+
+            # ASSERT
+            expect(page).to have_content('Saiu com sucesso')
+            expect(page).to_not have_content(user.email)
+            expect(page).to_not have_link('Logout')
+            expect(page).to have_link('Entrar como Locador')
         end
     end
-    
 end
