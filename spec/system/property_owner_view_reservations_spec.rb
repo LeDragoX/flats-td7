5d017cf4e5d3f9e1aad1dbf0078e0a667fbbe407
Plínio Larrubia <plinio2xd@gmail.com>
2021-11-03 16:28:18 -0300

    Fix date on all tests

--- spec/system/property_owner_view_reservations_spec.rb
+++ spec/system/property_owner_view_reservations_spec.rb
@@ -11,7 +11,7 @@ describe "Property owner view reservations" do
 
         andrew = User.create!(email: "andrew@doe.com.br", password: "123456")
 
-        andrews_reservation = PropertyReservation.create!(start_date: "10/10/2021", end_date: "14/10/2021",
+        andrews_reservation = PropertyReservation.create!(start_date: 5.days.from_now.to_date, end_date: 10.days.from_now.to_date,
                                     guests: 6, property: johns_property, user: andrew)
         
         login_as john, scope: :property_owner
@@ -22,8 +22,8 @@ describe "Property owner view reservations" do
         expect(page).to_not have_content "Reserve Agora"
         expect(page).to have_content "Reservas"
         expect(page).to have_content "Reserva de #{andrew.email}"
-        expect(page).to have_content "Data de Início: 10/10/2021"
-        expect(page).to have_content "Data de Saída: 14/10/2021"
+        expect(page).to have_content "Data de Início: #{I18n.l 5.days.from_now.to_date}"
+        expect(page).to have_content "Data de Saída: #{I18n.l 10.days.from_now.to_date}"
         expect(page).to have_content "Total de Pessoas: #{andrews_reservation.guests}"
         expect(page).to have_content "Status: Pendente"
     end
@@ -44,9 +44,9 @@ describe "Property owner view reservations" do
 
         andrew = User.create!(email: "andrew@doe.com.br", password: "123456")
 
-        PropertyReservation.create!(start_date: "10/10/2021", end_date: "14/10/2021",
+        PropertyReservation.create!(start_date: 5.days.from_now.to_date, end_date: 10.days.from_now.to_date,
                                     guests: 6, property: johns_property, user: andrew)
-        PropertyReservation.create!(start_date: "14/10/2021", end_date: "19/10/2021",
+        PropertyReservation.create!(start_date: 10.days.from_now.to_date, end_date: 15.days.from_now.to_date,
                                     guests: 6, property: janes_property, user: andrew)
         
         login_as john, scope: :property_owner
@@ -57,8 +57,8 @@ describe "Property owner view reservations" do
 
         expect(page).not_to have_content("Reserve Agora")
         expect(page).not_to have_content("Reserva de andrew@doe.com.br")
-        expect(page).not_to have_content("Data de Início: 13/10/2021")
-        expect(page).not_to have_content("Data de Saída: 16/10/2021")
+        expect(page).not_to have_content("Data de Início: #{I18n.l 5.days.from_now.to_date}")
+        expect(page).not_to have_content("Data de Saída: #{I18n.l 10.days.from_now.to_date}")
     end
 
     it "and accept reservation" do
@@ -71,7 +71,7 @@ describe "Property owner view reservations" do
 
         andrew = User.create!(email: "andrew@doe.com.br", password: "123456")
 
-        andrews_reservation = PropertyReservation.create!(start_date: "09/10/2021", end_date: "12/10/2021",
+        andrews_reservation = PropertyReservation.create!(start_date: 5.days.from_now.to_date, end_date: 10.days.from_now.to_date,
                                     guests: 6, property: johns_property, user: andrew)
         
         login_as john, scope: :property_owner
@@ -83,9 +83,9 @@ describe "Property owner view reservations" do
         expect(current_path).to eq property_path(johns_property)
         expect(page).to have_content "Reservas"
         expect(page).to have_content "Reserva de #{andrew.email}"
-        expect(page).to have_content 'Data de Início: 09/10/2021'
-        expect(page).to have_content 'Data de Saída: 12/10/2021'
-        expect(page).to have_content 'Total de Pessoas: 6'
-        expect(page).to have_content 'Status: Aceita'
+        expect(page).to have_content "Data de Início: #{I18n.l 5.days.from_now.to_date}"
+        expect(page).to have_content "Data de Saída: #{I18n.l 10.days.from_now.to_date}"
+        expect(page).to have_content "Total de Pessoas: 6"
+        expect(page).to have_content "Status: Aceita"
     end
 end
