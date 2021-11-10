08aca427bf5f4b1d6f1af453c5e4f21cd0be72c7
Plínio Larrubia <plinio2xd@gmail.com>
2021-11-10 14:44:16 -0300

    Add POST to Property creation, with testing and exception handling

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::PropertiesController < ActionController::API
+class Api::V1::PropertiesController < Api::V1::ApiController
   def index
     @properties = Property.all
     render json: @properties.as_json(except: %i[created_at updated_at property_type_id],
@@ -9,4 +9,19 @@ class Api::V1::PropertiesController < ActionController::API
     @property = Property.find(params[:id])
     render json: @property
   end
+
+  def create
+    @property = Property.create!(property_params)
+    render json: @property, status: 201
+  end
+  
+  private
+
+  def property_params
+    params.require(:property).permit(:title, :description, :rooms, :parking_slot,
+                                     :bathrooms, :pets,
+                                     :daily_rate,
+                                     :property_type_id, :property_owner_id)
+  end
+  
 end
--- spec/requests/property_api_spec.rb
+++ spec/requests/property_api_spec.rb
@@ -36,5 +36,50 @@ describe 'Property API' do
       expect(parsed_body[:daily_rate]).to eq('200.0')
       expect(parsed_body[:pets]).to eq(true)
     end
+
+    it 'should return 404 if property does not exist' do
+      get '/api/v1/properties/999'
+
+      expect(response).to have_http_status(404)
+      expect(parsed_body[:message]).to eq('Objeto não encontrado')
+    end
+    
+    it 'should return 500 if database is not available' do
+      property = create(:property, title: 'Apartamento legal', bathrooms: 5,
+                                   daily_rate: 200, pets: true)
+
+      allow(Property).to receive(:find).with(property.id.to_s)
+                                       .and_raise(ActiveRecord::ActiveRecordError)
+
+      get "/api/v1/properties/#{property.id}"
+
+      expect(response).to have_http_status(500)
+      expect(parsed_body[:message]).to eq('Falha ao consultar Banco de Dados')
+    end
   end
+
+  context 'POST /api/v1/properties' do
+    it 'create a new property' do
+      property_type = create(:property_type)
+      property_owner = create(:property_owner)      
+
+      property_params = { property: { title: 'Apartamento legal',
+                                      description: 'Um ap. legal',
+                                      rooms: 5, bathrooms: 1,
+                                      daily_rate: 100, pets: true,
+                                      property_type_id: property_type.id,
+                                      property_owner_id: property_owner.id } }
+      post '/api/v1/properties', params: property_params
+
+      expect(response).to have_http_status(201) 
+      expect(response.content_type).to include('application/json')
+      expect(parsed_body[:id]).to eq(Property.last.id)
+      expect(parsed_body[:title]).to eq('Apartamento legal')
+      expect(parsed_body[:rooms]).to eq(5)
+      expect(parsed_body[:bathrooms]).to eq(1)
+      expect(parsed_body[:daily_rate]).to eq('100.0')
+      expect(parsed_body[:pets]).to eq(true)
+    end
+  end
+  
 end
